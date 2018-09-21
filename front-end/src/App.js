33b20655d9ecd5eff7ca0730a2fd13644b0af133
Layth <laythq@gmail.com>
2018-09-21 17:56:18 +0100

    Tweets shown when button pressed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
@@ -10,14 +9,23 @@ class App extends Component {
       tweets: 'butt'
     }
     this.handleClick = this.handleClick.bind(this)
+    this.tweets = this.tweets.bind(this)
   }
 
   handleClick() {
     axios.get('http://localhost:3000/realDonaldTrump')
-      .then(response => this.setState({tweets: response.data[0].full_text}),
-      console.log(this.state.tweets));
+      .then(response => this.tweets(response));
   }
-  
+
+  tweets(input) {
+    const tweetHolder = ['tush']
+    console.log(input.data)
+    for (var i = 0; i < input.data.length; i++) {
+      const tweet = input.data[i]
+      tweetHolder.push(<p>{tweet.full_text}</p>)
+    }
+    this.setState({tweets: tweetHolder})
+  }
 
   render() {
     return (
