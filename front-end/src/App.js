003252c9862ea6baafd86f90a9fd104d09d884fe
Layth <laythq@gmail.com>
2018-09-22 11:27:11 +0100

    Added infinite scroll

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroller'
 
 class App extends Component {
   constructor () {
     super()
+    this.props = {
+      tweetHolder: '[]'
+    }
     this.state = {
-      tweets: 'butt'
+      tweets: 'butt',
+      last_tweet: ''
     }
     this.handleClick = this.handleClick.bind(this)
     this.tweets = this.tweets.bind(this)
+    this.loadMore = this.loadMore.bind(this)
   }
 
   handleClick() {
@@ -17,14 +23,24 @@ class App extends Component {
       .then(response => this.tweets(response));
   }
 
+  loadMore() {
+    axios.get('http://localhost:3000/realDonaldTrump')
+      .then(response => this.tweets(response));
+
+  }
+
   tweets(input) {
-    const tweetHolder = ['tush']
     console.log(input.data)
+    console.log(this.props.tweetHolder)
+    var arr = this.props.tweetHolder
     for (var i = 0; i < input.data.length; i++) {
       const tweet = input.data[i]
-      tweetHolder.push(<p>{tweet.full_text}</p>)
+      arr.push(<p>{tweet.full_text}</p>)
     }
-    this.setState({tweets: tweetHolder})
+    this.props.tweetHolder = arr
+    this.setState({tweets: this.props.tweetHolder})
+    this.setState({last_tweet: input.data[19]})
+    console.log('change')
   }
 
   render() {
@@ -32,6 +48,13 @@ class App extends Component {
       <div>
         <button onClick={this.handleClick}>Right here</button>
         <p>{this.state.tweets}</p>
+          <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadMore()}
+          hasMore={true || false}
+          loader={<div className="loader" key={0}>Loading ...</div>}
+          useWindow={false}
+          />
       </div>
     )
   }
